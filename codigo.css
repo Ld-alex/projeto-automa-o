4b51ac20e04954a3bf498f93a57fb12499319349
Ld-alex <86196975+Ld-alex@users.noreply.github.com>
2021-06-20 12:13:12 -0300

    codigo.css

---
+++ codigo.css
@@ -0,0 +1,157 @@
+raiz { 
+    display: bloco;
+}
+
+corpo {
+    margem: 0;
+    preenchimento: 0;
+}
+
+#embrulho {
+    acolchoamento superior: 40px;
+    posição: relativa;
+    largura: 530px;
+    altura: 600px;
+	margem direita: automático;
+	margem superior: 0;
+	margem esquerda: automático;
+        tamanho da fonte: 30px;
+        cor preta;
+        alinhamento de texto: centro;
+        família-fonte: arial;
+}
+
+div.rele1_ligado {
+    posição: absoluta;
+    direita: -110px;
+    topo: 220px;
+    largura: 300px;
+    altura: 206px;
+    fronteira: nenhum;
+		margem: automático;
+    fundo: url (lampada_ligada.png) sem repetição superior esquerdo;
+}
+
+div.rele1_desligado {
+    posição: absoluta;
+    direita: -110px;
+    topo: 220px;
+    largura: 300px;
+    altura: 206px;
+    fronteira: nenhum;
+	fundo: url (lampada_desligada.png) sem repetição superior esquerdo;
+}
+
+div.rele2_ligado {
+    posição: absoluta;
+    direita: -110px;
+    topo: 387px;
+    largura: 300px;
+    altura: 206px;
+    fronteira: nenhum;
+    fundo: url (lampada_ligada.png) sem repetição superior esquerdo;
+}
+
+div.rele2_desligado {
+    posição: absoluta;
+    direita: -110px;
+    topo: 387px;
+    largura: 300px;
+    altura: 206px;
+    fronteira: nenhum;
+	fundo: url (lampada_desligada.png) sem repetição superior esquerdo;
+}
+
+# div1 {
+	posição: absoluta;
+	direita: 85px;
+	superior: 195px;
+    raio da borda: 25px;
+    borda: 2px sólido # 000000;
+    largura: 350px;
+    altura: 150px; 
+}
+
+# div2 {
+	posição: absoluta;
+	direita: 85px;
+	superior: 355px;
+    raio da borda: 25px;
+    borda: 2px sólido # 000000;
+    largura: 350px;
+    altura: 150px; 
+}
+
+#botao {
+    posição: absoluta;
+    direita: 230px;
+    superior: 250px;
+	 largura: 200px;
+    altura: 256px;
+}
+
+.botao {
+	-moz-box-shadow: inserção 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow: inserção 0px 1px 0px 0px #ffffff;
+	box-shadow: inserção 0px 0px 0px 0px #ffffff;
+	plano de fundo: -webkit-gradiente (linear, superior esquerdo, inferior esquerdo, interrupção de cor (0,1, # 16ADFF), interrupção de cor (1, # 26A8FF));
+	fundo: gradiente -moz-linear (centro superior, # 16ADFF 5%, # 26A8FF 100%);
+	filtro: progid: DXImageTransform.Microsoft.gradient (startColorstr = '# 16ADFF', endColorstr = '# 26A8FF');
+	cor de fundo: #ededed;
+	-moz-border-radius: 6px;
+	-webkit-border-radius: 6px;
+	raio da borda: 6px;
+	borda: 1px sólido #dcdcdc;
+	display: bloco embutido;
+	cor: #ffffff;
+	família-fonte: arial;
+	tamanho da fonte: 28px;
+	intensidade da fonte: Negrito;
+	preenchimento: 10px 40px;
+	decoração de texto: nenhum;
+	sombra do texto: 0px 0px 0px #ffffff;
+        largura: 100px;
+        alinhamento de texto: centro;
+}
+
+} .botao: ativo {
+	posição: relativa;
+	topo: 1px;
+}
+
+# botao_2 {
+    posição: absoluta;
+    direita: 230px;
+    superior: 417px;
+	 largura: 200px;
+    altura: 256px;
+}
+
+.botao_2 {
+	-moz-box-shadow: inserção 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow: inserção 0px 1px 0px 0px #ffffff;
+	box-shadow: inserção 0px 0px 0px 0px #ffffff;
+	plano de fundo: -webkit-gradiente (linear, superior esquerdo, inferior esquerdo, interrupção de cor (0,1, # 16ADFF), interrupção de cor (1, # 26A8FF));
+	fundo: gradiente -moz-linear (centro superior, # 16ADFF 5%, # 26A8FF 100%);
+	filtro: progid: DXImageTransform.Microsoft.gradient (startColorstr = '# 16ADFF', endColorstr = '# 26A8FF');
+	cor de fundo: #ededed;
+	-moz-border-radius: 6px;
+	-webkit-border-radius: 6px;
+	raio da borda: 6px;
+	borda: 1px sólido #dcdcdc;
+	display: bloco embutido;
+	cor: #ffffff;
+	família-fonte: arial;
+	tamanho da fonte: 28px;
+	intensidade da fonte: Negrito;
+	preenchimento: 10px 40px;
+	decoração de texto: nenhum;
+	sombra do texto: 0px 0px 0px #ffffff;
+        largura: 100px;
+        alinhamento de texto: centro;
+}
+
+} .botao_2: ativo {
+	posição: relativa;
+	topo: 1px;
+}
